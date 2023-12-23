c869028095c33da516fb522a41ad1f56579c24d4
YumYummity <103061664+YumYummity@users.noreply.github.com>
2023-12-23 18:20:23 -0500

    fix dict

--- primebit/colors.py
+++ primebit/colors.py
@@ -1,6 +1,6 @@
 colors = {
-  error: '\x1b[31m',
-  success: '\x1b[32m',
-  logs: '\x1b[34m',
-  warning: '\x1b[33m',
+  "error": '\x1b[31m',
+  "success": '\x1b[32m',
+  "logs": '\x1b[34m',
+  "warning": '\x1b[33m',
 }
